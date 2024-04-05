ba6e910f4c1a94d21e3fb0ebcab8565db8329f47
wilson <weisenwilson.chew@etiqa.com.my>
2024-04-05 14:22:58 +0800

    add migration for adding 2 new lat and long columns

---
+++ Backend/WebApplication1/Migrations/20240405044149_AlterCity.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterCity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Cities",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Cities",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Cities");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Cities");
+        }
+    }
+}

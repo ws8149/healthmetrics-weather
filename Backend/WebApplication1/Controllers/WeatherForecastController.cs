aff65dd203d5d84e31d5700fd25fdd70920afe4f
wilson <weisenwilson.chew@etiqa.com.my>
2024-04-07 15:43:47 +0800

    implement caching mechanism + cache invalidation

--- Backend/WebApplication1/Controllers/WeatherForecastController.cs
+++ Backend/WebApplication1/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
@@ -10,37 +11,64 @@ namespace WebApplication1.Controllers
         
         private readonly ILogger<WeatherForecastController> _logger;
         private readonly IConfiguration _configuration;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
+        private readonly IDistributedCache _cache;
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration, IDistributedCache cache)
         {
             _logger = logger;
             _configuration = configuration;
+            _cache = cache;
         }
 
         // Get weather forecast for a specific city using OpenWeatherMap API
         [HttpPost("forecast")]
-        public async Task<IActionResult> WeatherForecast([FromBody] LocationData location)
-        {
-            var apiKey = _configuration["OpenWeatherApiKey"];
-            var url = $"https://api.openweathermap.org/data/2.5/forecast?lat={location.Latitude}&lon={location.Longitude}&appid={apiKey}&units=metric";
-            using (var client = new HttpClient())
+        public async Task<IActionResult> WeatherForecast([FromBody] CityDTO cityDto)
+        {   
+            // W-TODO: Handle bad requests and add try catch
+            
+            // Check if citydto id exists in redis cache
+            var cachedData = _cache.GetString(cityDto.Id.ToString());    
+            
+            if (cachedData != null)
             {
-                //W-TODO: Log err from OpenWeatherMap
-                var response = await client.GetAsync(url);
-                if (response.IsSuccessStatusCode)
+                return Ok(cachedData);
+            } else
+            {
+                var apiKey = _configuration["OpenWeatherApiKey"];
+
+                var url = $"https://api.openweathermap.org/data/2.5/forecast?lat={cityDto.Latitude}&lon={cityDto.Longitude}&appid={apiKey}&units=metric";
+                using (var client = new HttpClient())
                 {
-                    var content = await response.Content.ReadAsStringAsync();
-                    return Ok(content);
-                }
-                else
-                {
-                    return BadRequest("Failed to get weather forecast");
+                    //W-TODO: Log err from OpenWeatherMap
+                    var response = await client.GetAsync(url);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+
+                        // Calculate time until midnight
+                        DateTime now = DateTime.Now;
+                        DateTime midnight = now.Date.AddDays(1);
+                        TimeSpan timeUntilMidnight = midnight - now;
+
+                        // Cache data until midnight
+                        await _cache.SetStringAsync(cityDto.Id.ToString(), content, new DistributedCacheEntryOptions
+                        {
+                            AbsoluteExpirationRelativeToNow = timeUntilMidnight
+                        });
+                        return Ok(content);
+                    }
+                    else
+                    {
+                        return BadRequest("Failed to get weather forecast");
+                    }
                 }
             }
         }
 
-        public class LocationData
+        //W-TODO: move to models
+        public class CityDTO
         {
+            public int Id { get; set; }
+            public string Name { get; set; }
             public double Latitude { get; set; }
             public double Longitude { get; set; }
         }

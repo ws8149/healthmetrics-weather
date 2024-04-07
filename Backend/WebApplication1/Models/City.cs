eef80d4d07fb7cdda665565acc1dfce4a5f39195
wilson <weisenwilson.chew@etiqa.com.my>
2024-04-07 15:44:07 +0800

    add todo

--- Backend/WebApplication1/Models/City.cs
+++ Backend/WebApplication1/Models/City.cs
@@ -3,6 +3,7 @@
     public class City
     {
         public int Id { get; set; }
+        //W-TODO: make name required
         public string Name { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }

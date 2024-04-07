12bb9ee512f3210ad5e97b82e8d01fc088ad73d1
wilson <weisenwilson.chew@etiqa.com.my>
2024-04-07 16:09:21 +0800

    move city dto

---
+++ Backend/WebApplication1/Models/CityDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class CityDTO
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public required double Latitude { get; set; }
+        public required double Longitude { get; set; }
+    }
+}
